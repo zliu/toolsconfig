bdc2650062066faa0c3643f9dd0ff5ad2af9ff48
Zhuo Liu <lzhskywalker@gmail.com>
2013-09-11 13:59:10 +0800

    reorganize vim configs and scripts

--- scripts/u.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-find $PWD -name "*.[chlyS]" > cscope.files
-cscope -Rkqb -i cscope.files
-ctags -R --sort=foldcase  --fields=+S $PWD
--- update.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cp -u ~/.vimrc vimrc
-cp -u ~/.bashrc bashrc
-cp -u ~/.bash_profile bash_profile
-cp -ru ~/.vim/* vim/
